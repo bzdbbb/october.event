69fdb01c2bba299fd4bc527c0b6be97ec148ee67
bzdbbb <tim@chapelhse588.freeserve.co.uk>
2014-05-18 13:59:21 +0100

    Added properties

--- components/EventList.php
+++ components/EventList.php
@@ -2,6 +2,7 @@
 
 use Cms\Classes\ComponentBase;
 use bzdbbb\Event\Models\Event;
+use DB as Db;
 
 class EventList extends ComponentBase
 {
@@ -16,7 +17,23 @@ class EventList extends ComponentBase
 
     public function defineProperties()
     {
-        return [];
+        return [
+        'filter' => [
+            'title'             => 'Filter',
+            'type'              => 'dropdown',
+            'default'           => 'All',
+            'placeholder'       => 'Select filter',
+            'options'           => ["All" => "All"]
+            ],
+        'max' => [
+                'description'       => 'The most amount of event items allowed',
+                'title'             => 'Max items',
+                'default'           => 5,
+                'type'              => 'string',
+                'validationPattern' => '^[0-9]+$',
+                'validationMessage' => 'The Max Items value is required and should be integer.'
+            ]
+        ];
     }
 
     public function events()
