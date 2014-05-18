beadb75913c8471ab2f2e28321f6fecdab5046b0
bzdbbb <tim@chapelhse588.freeserve.co.uk>
2014-05-18 14:10:39 +0100

    Left aligned the date picker

--- formwidgets/datepicker/assets/js/datepicker.js
+++ formwidgets/datepicker/assets/js/datepicker.js
@@ -1,6 +1,6 @@
 /*
  * DatePicker plugin
- * 
+ *
  * Data attributes:
  * - data-control="datepicker" - enables the plugin on an element
  * - data-show-time="value" - allow the time to be chosen
@@ -11,48 +11,49 @@
  * JavaScript API:
  * $('a#someElement').datePicker({ option: 'value' })
  *
- * Dependences: 
+ * Dependences:
  * - Pikaday plugin (pikaday.js)
  * - Pikaday jQuery addon (pikaday.jquery.js)
  */
 
-+function ($) { "use strict";
++ function($) {
+    "use strict";
 
     // DATEPICKER CLASS DEFINITION
     // ============================
 
     var DatePicker = function(element, options) {
-        var self       = this
-        this.options   = options
-        this.$el       = $(element)
-        this.$input    = this.$el.find('input:first')
+        var self = this
+        this.options = options
+        this.$el = $(element)
+        this.$input = this.$el.find('input:first')
 
         // Init
 
         var $form = this.$el.closest('form'),
             changeMonitor = $form.data('oc.changeMonitor')
 
-        if (changeMonitor !== undefined)
-            changeMonitor.pause()
+            if (changeMonitor !== undefined)
+                changeMonitor.pause()
 
-        this.$input.pikaday({
-            showTime: options.showTime,
-            minDate: new Date(options.minDate),
-            maxDate: new Date(options.maxDate),
-            yearRange: options.yearRange,
-            defaultDate: new Date(),
-            onOpen: function() {
-                var $field = $(this._o.trigger)
+            this.$input.pikaday({
+                showTime: options.showTime,
+                minDate: new Date(options.minDate),
+                maxDate: new Date(options.maxDate),
+                yearRange: options.yearRange,
+                defaultDate: new Date(),
+                onOpen: function() {
+                    var $field = $(this._o.trigger)
 
-                $(this.el).css({
-                    left: 'auto',
-                    right: $(window).width() - $field.offset().left - $field.outerWidth()
-                })
-            }
-        })
+                    $(this.el).css({
+                        left: $field.offset().left,
+                        right: 'auto'
+                    })
+                }
+            })
 
-        if (changeMonitor !== undefined)
-            changeMonitor.resume()
+            if (changeMonitor !== undefined)
+                changeMonitor.resume()
     }
 
     DatePicker.DEFAULTS = {
@@ -67,11 +68,11 @@
 
     var old = $.fn.datePicker
 
-    $.fn.datePicker = function (option) {
+    $.fn.datePicker = function(option) {
         var args = Array.prototype.slice.call(arguments, 1)
-        return this.each(function () {
-            var $this   = $(this)
-            var data    = $this.data('oc.datepicker')
+        return this.each(function() {
+            var $this = $(this)
+            var data = $this.data('oc.datepicker')
             var options = $.extend({}, DatePicker.DEFAULTS, $this.data(), typeof option == 'object' && option)
             if (!data) $this.data('oc.datepicker', (data = new DatePicker(this, options)))
             else if (typeof option == 'string') data[option].apply(data, args)
@@ -83,7 +84,7 @@
     // DATEPICKER NO CONFLICT
     // =================
 
-    $.fn.datePicker.noConflict = function () {
+    $.fn.datePicker.noConflict = function() {
         $.fn.datePicker = old
         return this
     }
